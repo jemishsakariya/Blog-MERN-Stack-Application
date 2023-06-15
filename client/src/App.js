7247c1790e618be2b774f03d5fcf688b82bde7e9
Jemish sakariya <74782154+jemishsakariya@users.noreply.github.com>
2023-06-15 10:46:52 +0530

    Lazy and Suspense

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,43 @@
-import "./App.css";
+import { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home.js";
-import About from "./pages/about/About.js";
-import Blog from "./pages/blog/Blog";
-import Contact from "./pages/contact/Contact";
-import Dashboard from "./pages/dashboard/Dashboard";
-import Portfolio from "./pages/portfolio/Portfolio";
-import BlogPost from "./pages/blog/BlogPost";
-import Category from "./pages/portfolio/Category";
-import Login from "./pages/auth/login/Login";
-import Register from "./pages/auth/register/Register";
-import Navbar from "./components/navbar/Navbar";
-import Footer from "./components/footer/Footer";
+import "./App.css";
+
+const Home = lazy(() => import("./pages/Home"));
+const About = lazy(() => import("./pages/about/About"));
+const Blog = lazy(() => import("./pages/blog/Blog"));
+const Contact = lazy(() => import("./pages/contact/Contact"));
+const Dashboard = lazy(() => import("./pages/dashboard/Dashboard"));
+const Portfolio = lazy(() => import("./pages/portfolio/Portfolio"));
+const BlogPost = lazy(() => import("./pages/blog/BlogPost"));
+const Category = lazy(() => import("./pages/portfolio/Category"));
+const Login = lazy(() => import("./pages/auth/login/Login"));
+const Register = lazy(() => import("./pages/auth/register/Register"));
+const Navbar = lazy(() => import("./components/navbar/Navbar"));
+const Footer = lazy(() => import("./components/footer/Footer"));
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
 
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/blog/:id" element={<BlogPost />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/blog/:id" element={<BlogPost />} />
 
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/portfolio/:id" element={<Category />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/portfolio/:id" element={<Category />} />
 
-        <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/dashboard" element={<Dashboard />} />
 
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-      <Footer />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </Suspense>
     </>
   );
 }
