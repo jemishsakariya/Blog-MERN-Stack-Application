f9d2d3d1a9b88af451f3b86dd6fcbf10663c6eae
Jemish sakariya <74782154+jemishsakariya@users.noreply.github.com>
2023-06-15 13:09:06 +0530

    home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,25 @@
-import React from 'react'
-import styles from "./home.module.css"
+import React from "react";
+import styles from "./home.module.css";
+import hero from "../assets/hero.png";
 
 const Home = () => {
   return (
-    <div className={styles.container}>Home</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>
+          Better design for your digital products.
+        </h1>
+        <p className={styles.desc}>
+          Turning your Idea into Reality.We bring together the teams from the
+          global tech industry.
+        </p>
+        <button className={styles.button}>See Our Works</button>
+      </div>
+      <div className={styles.item}>
+        <img src={hero} alt="imgHero" className={styles.img} />
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
