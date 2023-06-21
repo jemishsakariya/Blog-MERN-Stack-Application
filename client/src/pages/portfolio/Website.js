cb367ecdb7219df2ddd5c4d8b834fc14f4867ba8
Jemish sakariya <74782154+jemishsakariya@users.noreply.github.com>
2023-06-21 08:49:41 +0530

    portfolio gallery content responsive

--- client/src/pages/portfolio/Website.js
+++ client/src/pages/portfolio/Website.js
@@ -17,7 +17,7 @@ const Website = () => {
     <>
       {web.map((item) => {
         return (
-          <div className={styles.item}>
+          <div className={styles.item} key={item.id}>
             <div className={styles.content}>
               <h1 className={styles.title}>{item.title}</h1>
               <p className={styles.desc}>{item.desc}</p>
